c3b5df5775b6443833504e81407a658ecce799ea
Allen Nobles <allennobles@mac.com>
2018-04-09 18:31:07 -0500

    Added refactor code files

---
+++ dominion/refactor.c
@@ -0,0 +1,30 @@
+#include "refactor.h"
+
+int adventurer(int card, int choice1, int choice2, int choice3,
+	       struct gameState *state, int handPos, int *bonus)
+{
+	return 0;
+}
+int smithy(int card, int choice1, int choice2, int choice3,
+	       struct gameState *state, int handPos, int *bonus)
+{
+	return 0;
+}
+
+int village(int card, int choice1, int choice2, int choice3,
+	       struct gameState *state, int handPos, int *bonus)
+{
+	return 0;
+}
+
+int garden(int card, int choice1, int choice2, int choice3,
+	       struct gameState *state, int handPos, int *bonus)
+{
+	return 0;
+}
+
+int council_room(int card, int choice1, int choice2, int choice3,
+	       struct gameState *state, int handPos, int *bonus)
+{
+	return 0;
+}
