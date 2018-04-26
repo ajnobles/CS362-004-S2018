e3c5b50632855129142e48e0914ef1e98607a1ae
Allen Nobles <allennobles@mac.com>
2018-04-26 16:49:10 -0500

    Switch Computers

--- projects/noblesal/dominion/unittest1.c
+++ projects/noblesal/dominion/unittest1.c
@@ -5,75 +5,134 @@
 #include "refactor.h"
 
 #include "rngs.h"
+#include <assert.h>
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
 int main (int argc, char *argv[])
 {
-    struct gameState *test = malloc(sizeof(struct gameState));
-    if(!test) {
-      printf("gameState test failed to malloc\n");
-      return 1;
+    // GAME SETUP
+    int numPlayers = 2;
+    int kingdomCards[10] = {adventurer, smithy, gardens, village, council_room, minion, steward, great_hall, tribute, ambassador};
+    int seed = 1000;
+
+    struct gameState G, testG;
+
+    int i,
+        j,
+        testGameOver,
+        gameOver = 0,
+        testNumber = 1;
+
+    initializeGame(numPlayers, kingdomCards, seed, &G);
+
+    // TEST 1
+    printf("TEST %i STARTED\n", testNumber);
+    printf("Initial gameState Settings\n");
+
+    memcpy(&testG, &G, sizeof(struct gameState));
+    testGameOver = isGameOver(&testG);
+
+    // Test isGameOver to G
+    if (G.supplyCount[province] == 0) {
+        gameOver = 1;
     }
 
-    int i, j, n;
-    int testCases[] = {-100, 0, 1, 10, 40};
-
-    n = sizeof(testCases)/sizeof(testCases[0]);
-
-    for (i = 0; i < n; i++) {
-      test->numPlayers = testCases[i];
-
-      for (j=0; j < testCases[i];j++) {
-        test->whoseTurn = j;
-
-        if (whoseTurn(test) != j) {
-          printf("TEST FAILED\n");
-          exit(1);
+    j = 0;
+    for (i = 0; i < 25; i++) {
+        if (G.supplyCount[i] == 0) {
+            j++;
         }
-      }
+
+        if (j >= 3) {
+            gameOver = 1;
+        }
     }
 
-    //
-    // // TEST 1
-    // numPlayersTest = 4;
-    // test->numPlayers = numPlayersTest;
-    // for(i = 0; i < numPlayersTest; i++) {
-    //   test->whoseTurn = i;
-    //
-    //   if(whoseTurn(test) != i) {
-    //     printf("TEST FAILED\n");
-    //     exit(1);
-    //   }
-    // }
-    //
-    // // TEST 2
-    // numPlayersTest = 0;
-    // test->numPlayers = numPlayersTest;
-    // for(i = 0; i < numPlayersTest; i++) {
-    //   test->whoseTurn = i;
-    //
-    //   if(whoseTurn(test) != i) {
-    //     printf("TEST FAILED\n");
-    //     exit(1);
-    //   }
-    // }
-    //
-    // // TEST 3
-    // numPlayersTest = -1;
-    // test->numPlayers = numPlayersTest;
-    // for(i = 0; i < numPlayersTest; i++) {
-    //   test->whoseTurn = i;
-    //
-    //   if(whoseTurn(test) != i) {
-    //     printf("TEST FAILED\n");
-    //     exit(1);
-    //   }
-    // }
+    printf("isGameOver: %i, expected: %i\n", testGameOver, gameOver);
+    printf("supplyCount[province]: %i, expected: %i\n", testG.supplyCount[province], G.supplyCount[province]);
+    assert(gameOver == testGameOver);
+    assert(G.supplyCount[province] == testG.supplyCount[province]);
+    for (i = 0; i < 25; i++) {
+        assert(G.supplyCount[i] == testG.supplyCount[i]);
+    }
+    printf("TEST %i PASSED\n\n", testNumber++);
 
-    printf("TEST PASSED\n");
+
+    printf("TEST %i STARTED\n", testNumber);
+    G.supplyCount[adventurer] = 0;
+    G.supplyCount[smithy] = 0;
+    G.supplyCount[village] = 0;
+    G.supplyCount[province] = 0;
+    gameOver = 0;
+
+    memcpy(&testG, &G, sizeof(struct gameState));
+    testGameOver = isGameOver(&testG);
+
+    // Test isGameOver to G
+    if (G.supplyCount[province] == 0) {
+        gameOver = 1;
+    }
+
+    j = 0;
+    for (i = 0; i < 25; i++) {
+        if (G.supplyCount[i] == 0) {
+            j++;
+        }
+
+        if (j >= 3) {
+            gameOver = 1;
+        }
+    }
+
+    printf("isGameOver: %i, expected: %i\n", testGameOver, gameOver);
+    printf("supplyCount[province]: %i, expected: %i\n", testG.supplyCount[province], G.supplyCount[province]);
+    assert(gameOver == testGameOver);
+    assert(G.supplyCount[province] == testG.supplyCount[province]);
+    for (i = 0; i < 25; i++) {
+        assert(G.supplyCount[i] == testG.supplyCount[i]);
+    }
+    printf("TEST %i PASSED\n\n", testNumber++);
+
+
+    printf("TEST %i STARTED\n", testNumber);
+    G.supplyCount[adventurer] = 0;
+    G.supplyCount[smithy] = 0;
+    G.supplyCount[village] = 0;
+    G.supplyCount[province] = 4;
+    gameOver = 0;
+
+    memcpy(&testG, &G, sizeof(struct gameState));
+    testGameOver = isGameOver(&testG);
+
+    // Test isGameOver to G
+    if (G.supplyCount[province] == 0) {
+        gameOver = 1;
+    }
+
+    j = 0;
+    for (i = 0; i < 25; i++) {
+        if (G.supplyCount[i] == 0) {
+            j++;
+        }
+
+        if (j >= 3) {
+            gameOver = 1;
+        }
+    }
+
+    printf("isGameOver: %i, expected: %i\n", testGameOver, gameOver);
+    printf("supplyCount[province]: %i, expected: %i\n", testG.supplyCount[province], G.supplyCount[province]);
+    assert(gameOver == testGameOver);
+    assert(G.supplyCount[province] == testG.supplyCount[province]);
+    for (i = 0; i < 25; i++) {
+        assert(G.supplyCount[i] == testG.supplyCount[i]);
+    }
+    printf("TEST %i PASSED\n\n", testNumber++);
+
     return 0;
 }
