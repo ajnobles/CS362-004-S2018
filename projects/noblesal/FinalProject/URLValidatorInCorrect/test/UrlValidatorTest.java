d3285cf3e7100663d6dc0a92b98a2f0b9a0d39b2
Allen Nobles <allennobles@mac.com>
2018-06-09 16:43:19 -0500

    Final Project Setup

--- projects/noblesal/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/noblesal/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -41,9 +41,131 @@ public class UrlValidatorTest extends TestCase {
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
+	   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   //boolean test = urlVal.isValid("http://ww.google.com");
+	   //System.out.print(test + "\n");
+	   
+	   boolean expectedResult;
+	   boolean actualResult; 
+	   /*
+	   String[] trueURL = {"http://www.google.com:80?action=view", 
+			   "https://www.google.com:80?action=view",
+			   "http://www.google.com:80?action=view",
+			   "http://www.google.com?action=view"};
+	   
+	   String[] falseURL = {"http:///aaa.google.com:80?action=view",
+			   "http://www.google.com:-1?action=view",
+			   "http://www.google.m:80?action=view",
+			   "http:///www.google.com:-1?action=view",
+			   "http://www.google.com?80?action=view"};
+	   
+	   
+	   // TRUE TEST LOOP
+	   expectedResult = true;
+	   for (String s: trueURL) {
+		   actualResult = urlVal.isValid(s);
+		   
+		   if (expectedResult != actualResult) {
+			   System.out.print(s + "\tTEST FAILED\n");
+		   }
+	   }
+	  
+	   // FALSE TEST LOOP
+	   expectedResult = false;
+	   for (String s: falseURL) {
+		   actualResult = urlVal.isValid(s);
+		   
+		   if (expectedResult != actualResult) {
+			   System.out.print(s + "\tTEST FAILED\n");
+		   }
+	   }
+	   */
+	   
+	   // ftp;// causing error
+	   String[] scheme = {
+			   "http://", 
+			   "http??",
+			   "http://"
+			   };
+	   
+	   String[] authority = {
+			   "www.google.com",
+			   "ww.google.com",
+			   "go.au",
+			   "go.a",
+			   "255.255.255.255",
+			   "255.255.256.256"
+			   };
+	   
+	   String[] port = {
+			   ":80", 
+			   ":-1"
+			   };
+	   
+	   String[] path = {
+			   "/test1", 
+			   "/..",
+			   "",
+			   "//.."			   
+			   };
+	      
+	   int sCase = 0;
+	   int aCase = 0;	   
+	   int poCase = 0;
+	   int paCase = 0;
+	   int numTest = 0;
 
+	   for (String s : scheme) {
+		   
+		   
+		   for (String a : authority) {
+
+			   
+			   for (String po : port) {
+
+
+				   for (String pa : path) {
+
+
+					   expectedResult = (sCase % 2 == 0 && aCase % 2 == 0 
+							   && poCase % 2 == 0 && paCase % 2 == 0) 
+							   ? true : false;
+
+					   
+					   StringBuilder testUrl = new StringBuilder();
+					   testUrl.append(s);
+					   testUrl.append(a);
+					   testUrl.append(po);
+					   testUrl.append(pa);
+					   
+					   String url = testUrl.toString();
+
+					   actualResult = urlVal.isValid(url);
+
+					   if (expectedResult != actualResult) {
+						   System.out.print(url + "\tTEST FAILED"+ "\tExpected " + expectedResult + "\tReturned " + actualResult + "\n");
+					   }
+					   
+					   //System.out.print(url + "\tExpected " + expectedResult + "\tReturned " + actualResult + "\n");
+
+					   numTest++;
+					   paCase++;
+				   }
+				   
+				   poCase++;
+			   }
+			   
+			   aCase++;
+		   }
+		   
+		   sCase++;
+	   }
+	   
+	   System.out.print("numTest: " + numTest + "\n");
    }
+}
+   
    
 
-
-}
